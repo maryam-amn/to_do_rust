b39a4fcb67f18b36473a79813ce3e3d8d0c20dac
maryam-amn <maryam.Aman@jobtrek.ch>
2024-10-04 09:46:50 +0200

    feat: 4.2 write a todo list in a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+
+use std::io::{self, Write};
 fn main() {
-    println!("Hello, world!");
+    
+
+    let mut todo = String::new();
+
+    println!("Veuillez faire votre to-do list :");
+
+    let mut text = File::create("text.txt").expect("creation failed");
+    
+    io::stdin().read_line(&mut todo).expect("Read line failed.");
+    
+    // Write contents to the file
+    text.write(todo.as_bytes()).expect("write failed");
+
+   
 }
+
