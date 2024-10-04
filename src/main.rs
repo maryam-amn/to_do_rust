2cb86bc8ad45030416cd41db64c4a408b13a60ee
maryam-amn <maryam.Aman@jobtrek.ch>
2024-10-04 10:58:43 +0200

    feat: 4.3 add a new line in the to-do list

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,27 @@ fn main() {
 
     let mut todo = String::new();
 
+    let mut todo2 = String::new();
+
+
     println!("Veuillez faire votre to-do list :");
 
     let mut text = File::create("text.txt").expect("creation failed");
-    
+    // prend le inpu et
+
     io::stdin().read_line(&mut todo).expect("Read line failed.");
-    
+
     // Write contents to the file
     text.write(todo.as_bytes()).expect("write failed");
 
-   
+
+    // Ajouter une to do 
+    io::stdin().read_line(&mut todo2).expect("Read line failed.");
+
+    text.write(todo2.as_bytes()).expect("write failed");
+
+    
 }
+
+
 
