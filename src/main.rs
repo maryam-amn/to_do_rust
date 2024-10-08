e2059b8ca1bb8ed2d5f6f9802be0f0c55b92a9fe
maryam-amn <maryam.Aman@jobtrek.ch>
2024-10-08 12:57:56 +0200

    feat: 4.3 add a new line in the file of the to do list

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
-use std::fs::File;
-
 use std::io::{self, Write};
+
+use std::fs::OpenOptions;
+
 fn main() {
-    
-
     let mut todo = String::new();
-
-    let mut todo2 = String::new();
-
 
     println!("Veuillez faire votre to-do list :");
 
-    let mut text = File::create("text.txt").expect("creation failed");
-    // prend le inpu et
+    // Open the file 
+    let mut text = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("text.txt")
+        .expect("cannot open file");
 
     io::stdin().read_line(&mut todo).expect("Read line failed.");
 
-    // Write contents to the file
-    text.write(todo.as_bytes()).expect("write failed");
+    let todo: &str = todo.trim();
 
-
-    // Ajouter une to do 
-    io::stdin().read_line(&mut todo2).expect("Read line failed.");
-
-    text.write(todo2.as_bytes()).expect("write failed");
-
-    
+    println!("message ");
+    writeln!(text, "{}", todo).expect("cannot open file");
 }
-
-
-
