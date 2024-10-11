ef04ddb6474a1295de8496fbf56e07c33467fc6c
maryam-amn <maryam.Aman@jobtrek.ch>
2024-10-11 13:43:59 +0200

    feat: 4.4 add a delete flag

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-use std::io::{self, Write};
+use std::fs::{self, read_to_string};
+use std::io::{self};
 
-use std::fs::OpenOptions;
-
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut todo = String::new();
 
-    println!("Veuillez faire votre to-do list :");
-
-    // Open the file 
-    let mut text = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open("text.txt")
-        .expect("cannot open file");
-
+    println!("Enter the to-do list:");
     io::stdin().read_line(&mut todo).expect("Read line failed.");
 
     let todo: &str = todo.trim();
 
-    println!("message ");
-    writeln!(text, "{}", todo).expect("cannot open file");
+    let mut todos: Vec<String> = match read_to_string("text.txt") {
+        Err(_) => Vec::new(),
+        Ok(text) => text.lines().map(String::from).collect(),
+    };
+
+    if todo.contains("--delete") {
+        let test = todo.split("pat").last();
+        let number_line: usize = test.expect("Err").parse().unwrap();
+
+        todos.remove(number_line - 1);
+    } else {
+        todos.push(todo.to_string())
+    };
+
+    fs::write("text.txt", todos.join("\n")).expect("can't write");
+
+    Ok(())
 }
