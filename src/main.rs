d7fe2801db74d4b23ba3db819c975ae9bae84f9f
maryam-amn <maryam.Aman@jobtrek.ch>
2024-10-23 08:11:22 +0200

    feat!:change of the text file into a Json file

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
 use std::fs::{self, read_to_string};
 use std::io::{self};
 
+use serde::{Deserialize, Serialize};
+use serde_json;
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    content: String,
+}
 fn main() -> std::io::Result<()> {
-    let mut todo = String::new();
+    let mut user_input = String::new();
 
     println!("Enter the to-do list:");
-    io::stdin().read_line(&mut todo).expect("Read line failed.");
 
-    let todo: &str = todo.trim();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Read line failed.");
 
-    let mut todos: Vec<String> = match read_to_string("text.txt") {
+    let mut todos: Vec<Todo> = match read_to_string("todo.json") {
+        Ok(file_content) => serde_json::from_str(&file_content).expect("Cannot deseraliaze Json"),
         Err(_) => Vec::new(),
-        Ok(text) => text.lines().map(String::from).collect(),
     };
 
-    if todo.contains("--delete") {
-        let test = todo.split_whitespace().last();
-        let number_line: usize = test.expect("Err").parse().unwrap();
+    let user_input = user_input.trim();
 
-        todos.remove(number_line - 1);
+    // delete flag
+    if user_input.contains("--delete") {
+        let test = user_input.split(" ").last().expect("Cannot find number");
+        //verifie si il y a un nombre
+        let todo_number: usize = test.parse().expect("Cannot convert number");
+
+        todos.remove(todo_number - 1);
     } else {
-        todos.push(todo.to_string())
-    };
-
-    fs::write("text.txt", todos.join("\n")).expect("can't write");
+        todos.push(Todo {
+            content: user_input.to_string(),
+        });
+    }
+    // Write in the Json file
+    fs::write("todo.json", serde_json::to_string(&mut todos).expect("err")).expect("can't write");
 
     Ok(())
 }
