ffd59835c90b8a56a8037ff0e1600ef21937d572
maryam-amn <maryam.Aman@jobtrek.ch>
2024-10-24 08:29:04 +0200

    feat: 4.6 we can use flag and argument in the command line

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,47 @@
 use std::fs::{self, read_to_string};
 use std::io::{self};
-
 use serde::{Deserialize, Serialize};
 use serde_json;
+use clap::Parser;
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Todo {
     content: String,
 }
+#[derive(Parser,Debug)]
+#[command(version = "0.1")]
+struct Flag {
+    #[arg(long, short)]
+    delete: Option<usize>
+}
 fn main() -> std::io::Result<()> {
-    let mut user_input = String::new();
-
-    println!("Enter the to-do list:");
-
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Read line failed.");
+    
+    let flags = Flag::parse();
 
     let mut todos: Vec<Todo> = match read_to_string("todo.json") {
         Ok(file_content) => serde_json::from_str(&file_content).expect("Cannot deseraliaze Json"),
         Err(_) => Vec::new(),
     };
 
-    let user_input = user_input.trim();
-
     // delete flag
-    if user_input.contains("--delete") {
-        let test = user_input.split(" ").last().expect("Cannot find number");
-        //verifie si il y a un nombre
-        let todo_number: usize = test.parse().expect("Cannot convert number");
-
-        todos.remove(todo_number - 1);
+  
+    if let Some(number_line) = flags.delete {
+        if number_line > 0 && number_line <= todos.len() {
+            todos.remove(number_line - 1);
+        } 
     } else {
-        todos.push(Todo {
-            content: user_input.to_string(),
-        });
+        let mut user_input = String::new();
+        println!("Enter a to-do list");
+        io::stdin().read_line(&mut user_input)?;
+
+        let user_input = user_input.trim();
+        if !user_input.is_empty() {
+            todos.push(Todo {
+                content: user_input.to_string(),
+            });
+            } 
     }
-    // Write in the Json file
+    
     fs::write("todo.json", serde_json::to_string(&mut todos).expect("err")).expect("can't write");
 
     Ok(())
