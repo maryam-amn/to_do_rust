e76e94a6ff568ceb095db48a7ada0299076c7e03
maryam-amn <maryam.Aman@jobtrek.ch>
2024-10-25 14:13:28 +0200

    feat: 4.10 Add a new flag that sorts the list in priority order.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use serde_json::{self};
 use std::fs::{self, read_to_string};
 use std::io::{self};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Ord, Eq, PartialOrd, PartialEq)]
 struct Todo {
     content: String,
     status: bool,
@@ -13,26 +13,30 @@ struct Todo {
 }
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
+// we use the clap library in order to be able to automatically manage the arguments and flags of the
+//command line.
 struct Flag {
     #[arg(long, short, default_value_t = 0)]
     // to indicate that a to-do can be delete
     delete: usize,
-    // Option<usize>
     // to indicate that the to do is finished
     #[arg(long, default_value_t = 0, short = 'D')]
-    done: usize, //bool
+    done: usize, 
     // to indicate that the to-do is unfinshed 3 line
-    #[arg(long, default_value_t = 0, short = 'u')]
-    undone: usize, //bool
+    #[arg(long, default_value_t = 0, )]
+    undone: usize, 
     // to indicate we can have a deadline
     #[arg(long)]
     due: Option<String>,
-
+    // to indicate id, or the line of the to-do
     #[arg(long, default_value_t = 0)]
     id: usize,
-
-    #[arg(long,)]
-    list: Option<bool>,
+    // to do a list of the to-do
+    #[arg(long)]
+    list: bool,
+    // to sort the to-do by order of priority
+    #[arg(short, long)]
+    sort: bool,
 }
 
 fn main() -> std::io::Result<()> {
@@ -42,31 +46,42 @@ fn main() -> std::io::Result<()> {
         Ok(file_content) => serde_json::from_str(&file_content).expect("Cannot deseraliaze Json"),
         Err(_) => Vec::new(),
     };
-
-    // there is a  delete flag who remove a elements
-
+    // Here the delete flag remove a elements
     if flags.delete > 0 && flags.delete <= todos.len() {
         todos.remove(flags.delete - 1);
-    } else if flags.done > 0 && flags.done <= todos.len() {
+    }
+    // there is a flage done who indicate that the task is completed
+    else if flags.done > 0 && flags.done <= todos.len() {
         todos[flags.done - 1].status = true;
-    } else if flags.undone > 0 && flags.undone <= todos.len() {
+    }
+    // the flag undone indicate that the task is not done
+    else if flags.undone > 0 && flags.undone <= todos.len() {
         todos[flags.undone - 1].status = false;
-    } else if let Some(due_date) = flags.due {
+    }
+    // The flag due indicate a deadline
+    else if let Some(due_date) = flags.due {
         match NaiveDate::parse_from_str(&due_date, "%Y-%m-%d") {
             Ok(date) => {
                 todos[flags.id - 1].deadline = Some(date);
             }
             Err(_) => {
-                println!("Mauvais format");
+                println!("Invalid format, try again");
             }
         }
-    } else if  flags.list.expect("Erreur de commande") {
-            
-            for (i, list_todo) in todos.iter().enumerate() {
+    }
+    // the flag list, show us the list of  all the to-do and their status
+    else if flags.list {
+        for (i, list_todo) in todos.iter().enumerate() {
             let status = if list_todo.status { "Done" } else { "Undone" };
             println!("{}. {}, {}", i + 1, list_todo.content, status);
-            }
-    }else {
+        }
+    }
+    // the flag sort, it sorts the to-do list in priority order
+    else if flags.sort {
+        todos.sort_by(|a, b| a.deadline.cmp(&b.deadline));
+    }
+    // if there is no flag, the input of the user will be written in the to-do list
+    else {
         let mut user_input = String::new();
         println!("Enter a to-do list");
         io::stdin().read_line(&mut user_input)?;
@@ -80,6 +95,7 @@ fn main() -> std::io::Result<()> {
         todos.push(user_todo);
     }
 
+    // Here the input of the user will be written in the todo json file
     fs::write(
         "todo.json",
         serde_json::to_string_pretty(&todos).expect("err"),
