4593174c2944c4d0ec15fb23c9369baa20d3a8df
maryam-amn <maryam.Aman@jobtrek.ch>
2024-10-25 08:51:24 +0200

    feat: 4.9 Add a new flag wich allows us to have a deadline with this date  format YY-MM-DD

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDate;
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 use serde_json::{self};
@@ -8,18 +9,27 @@ use std::io::{self};
 struct Todo {
     content: String,
     status: bool,
+    deadline: Option<NaiveDate>,
 }
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Flag {
     #[arg(long, short, default_value_t = 0)]
+    // to indicate that a to-do can be delete
     delete: usize,
+    // Option<usize>
     // to indicate that the to do is finished
+    #[arg(long, default_value_t = 0, short = 'D')]
+    done: usize, //bool
+    // to indicate that the to-do is unfinshed 3 line
+    #[arg(long, default_value_t = 0, short = 'u')]
+    undone: usize, //bool
+    // to indicate we can have a deadline
+    #[arg(long)]
+    due: Option<String>,
+
     #[arg(long, default_value_t = 0)]
-    done: usize,
-    // to indicate that the to-do is unfinshed 3 line 
-    #[arg(long, default_value_t = 0)]
-    undone: usize,
+    id: usize,
 }
 
 fn main() -> std::io::Result<()> {
@@ -30,17 +40,24 @@ fn main() -> std::io::Result<()> {
         Err(_) => Vec::new(),
     };
 
-    // delete flag
+    // there is a  delete flag who remove a elements
 
     if flags.delete > 0 && flags.delete <= todos.len() {
         todos.remove(flags.delete - 1);
     } else if flags.done > 0 && flags.done <= todos.len() {
         todos[flags.done - 1].status = true;
-    }
-    else if flags.undone > 0 && flags.undone <= todos.len() {
-        todos[flags.done - 1].status = false;
-    }
-     else {
+    } else if flags.undone > 0 && flags.undone <= todos.len() {
+        todos[flags.undone - 1].status = false;
+    } else if let Some(due_date) = flags.due {
+        match NaiveDate::parse_from_str(&due_date, "%Y-%m-%d") {
+            Ok(date) => {
+                todos[flags.id - 1].deadline = Some(date);
+            }
+            Err(_) => {
+                println!("Mauvais format");
+            }
+        }
+    } else {
         let mut user_input = String::new();
         println!("Enter a to-do list");
         io::stdin().read_line(&mut user_input)?;
@@ -49,11 +66,16 @@ fn main() -> std::io::Result<()> {
         let user_todo = Todo {
             content: user_input.to_string(),
             status: false,
+            deadline: None,
         };
         todos.push(user_todo);
     }
 
-    fs::write("todo.json", serde_json::to_string(&todos).expect("err")).expect("can't write");
+    fs::write(
+        "todo.json",
+        serde_json::to_string_pretty(&todos).expect("err"),
+    )
+    .expect("can't write");
 
     Ok(())
 }
